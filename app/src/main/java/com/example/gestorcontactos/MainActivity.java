a7a13f47e09faf9f57a0cf350ffbc0841c525d64
Kevin Molina <kevinmolina.dev@gmail.com>
2023-06-15 10:45:09 -0400

    Agregando mejoraras y pantallas

--- app/src/main/java/com/example/gestorcontactos/MainActivity.java
+++ app/src/main/java/com/example/gestorcontactos/MainActivity.java
@@ -1,71 +1,96 @@
 package com.example.gestorcontactos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
 import com.example.gestorcontactos.Pantallas.about_us;
 import com.example.gestorcontactos.Pantallas.contactos;
 import com.example.gestorcontactos.Pantallas.favorite;
+import com.example.gestorcontactos.Pantallas.tag;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
+    SearchView search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        DrawerLayout drawerLayout;
-        BottomNavigationView bottomNavigationView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Inicio();
+        busqueda();
+    }
+    public void Inicio() {
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open,
                 R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new contactos()).commit();
-            navigationView.setCheckedItem(R.id.nav_home);
-        }
-        replaceFragment(new contactos());
 
+        replaceFragment(new contactos());
         bottomNavigationView.setOnItemSelectedListener(item -> {
-            /*switch (item.getItemId()) {
-                case R.id.contact:
-                    replaceFragment(new contactos());
-                    break;
-                case R.id.favorite:
-                    replaceFragment(new favorite());
-                    break;
-                case R.id.about_us:
-                    replaceFragment(new about_us());
-                    break;
-            }*/
             int itemId = item.getItemId();
             if (itemId == R.id.contact) {
                 replaceFragment(new contactos());
             } else if (itemId == R.id.favorite) {
                 replaceFragment(new favorite());
-            } else if (itemId == R.id.about_us) {
-                replaceFragment(new about_us());
             }
             return true;
         });
     }
-    private  void replaceFragment(Fragment fragment) {
+    /*getSupportFragmentManager().beginTransaction().replace(R.id.drawer_layout, new tag()).commit();
+            navigationView.setCheckedItem(R.id.casa);*/
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.fragment_container, fragment);
         fragmentTransaction.commit();
     }
 
+    public void busqueda() {
+        search = findViewById(R.id.search);
+        search.setOnQueryTextListener(this);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        System.out.println("Texto de búsqueda: " + query);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
+
+    public void tag_close(View view) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.drawer_layout);
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().remove(fragment).commit();
+        }
+    }
 }
+
+
